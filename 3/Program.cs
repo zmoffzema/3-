911de3f5256f3299f16107413107eb9e1bf183f2
z_moff <zmoffzema@gmail.com>
2024-06-20 00:08:40 +0300

    Добавьте файлы проекта.

---
+++ 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Заданный массив чисел
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        // Находим сумму элементов массива
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        // Вычисляем среднее арифметическое значение
+        double average = (double)sum / numbers.Length;
+
+        // Выводим результаты на экран
+        Console.WriteLine("Сумма элементов массива: " + sum);
+        Console.WriteLine("Среднее арифметическое значение элементов массива: " + average);
+    }
+}
